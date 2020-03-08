f30a0bce8ee2df6511a5ef99c08463e247a5e95b
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-08 18:19:05 -0400

    preeti_code

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Calculator:
 
     def __init__(self):
@@ -7,6 +9,58 @@ class Calculator:
         return x + y
 
     def sub(self, x, y):
-        return 0
+        return x - y
+    def __mul__(self, x, y):
+        return x * y
+
+
+
 
 # add lots more methods to this calculator class.
+
+#Preeti
+    def square(self,x):
+        result = x ** 2
+        print ("Square = ", result)
+
+
+    def squareroot(self,x):
+        result = math.sqrt(x)
+        print ("Squareroot = ", result)
+
+
+    def exponential(self,x, y):
+        result = x ** y
+        print ("Exponential = ", result)
+
+
+    def sine(self,x):
+        result = math.sin()
+        print ("sin = ", result)
+
+
+    def cosine(self,x):
+        result = math.cos()
+        print ("cos = ", result)
+
+
+    def tang(self,x):
+        result = math.tan()
+        print ("tan = ", result)
+
+
+    def asine(self,x):
+        result = math.asin()
+        print ("inverse sin = ", result)
+
+
+    def acosine(self,x):
+        result = math.acos()
+        print ("inverse cos = ", result)
+
+
+    def atang(self,x):
+        result = math.atan()
+        print ("inverse tan = ", result)
+
+
