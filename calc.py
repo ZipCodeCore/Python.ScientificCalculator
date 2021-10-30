9d08e210bef0f08487173df2b1215b923218d1e2
Drake Dwornik <drake@drakedwornik.com>
2021-10-29 22:23:24 -0400

    capitalized the command input

--- calc.py
+++ calc.py
@@ -11,17 +11,19 @@ while True:
     state.print()
     if state.command != None:
         print("current command is:", state.command)
-    state.user_entry = input("please enter a mathematical function or number: ")
+    state.user_entry = input("please enter a mathematical function or number (or type help): ")
     if state.user_entry.isnumeric():
         state.user_entry = float(state.user_entry)
         if state.command == None:
             state.value = state.user_entry
-        elif state.command == "add":
+        elif state.command == "+":
             state.add(state.user_entry)
-        elif state.command == "sub":
+        elif state.command == "-":
             state.sub(state.user_entry)
-    elif state.user_entry == "exit":
+        elif state.command == "HELP":
+            state.help()
+    elif state.user_entry == "EXIT":
         break
     else:
-        state.command = state.user_entry
+        state.command = state.user_entry.capitalize()
 print("thanks for pushing all my buttons")
