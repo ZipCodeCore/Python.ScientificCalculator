74ea349245a9ff55c4bb0b3a8c269e2717edbc4b
TomLafferty <zipcode0>
2021-10-30 00:39:52 -0400

    functioning base calculator with input and functions from math.

---
+++ dev.py
@@ -0,0 +1,17 @@
+import math
+
+
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def sub(self, x, y):
+        return 0
+
+
+# add lots more methods to this calculator class.
+
