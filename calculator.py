f53afaf58755dd93cd94ad3a7a68b8e2195825c7
Roethel Christine <roethelchristine@Dipintis-MacBook-Pro.local>
2021-10-30 15:46:02 -0400

    CR test 1

--- calculator.py
+++ calculator.py
@@ -1,12 +1,41 @@
-class Calculator:
+import math
 
+class Calculator:
     def __init__(self):
         pass
 
-    def add(self, x, y):
+    def add(x, y):
+        print(x + y)
         return x + y
 
-    def sub(self, x, y):
-        return 0
+    def subtract(x, y):
+        print(x - y)
+        return x - y
 
-# add lots more methods to this calculator class.
+    def multiply(x, y):
+        print(x * y)
+        return y * x
+
+    def divide(x, y):
+        print(x / y)
+        return x / y
+
+
+    def square(base):
+        print(base ** 2)
+        return base ** 2
+
+
+    def exp(x, y):
+        print(x ** y)
+        return x ** y
+
+
+    def square_root(x):
+        print(x ** (1 / 2))
+        return x ** (1 / 2)
+
+
+    def inv(x):
+        print(1 / x)
+        return 1 / x
--- main-app.py
+++ main-app.py
@@ -1,34 +1,107 @@
 from calculator import Calculator
+import math
+import decimal
 
+global mem
+mem = 0
 
-def getTwoNumbers():
-    a = float(input("first number? "))
-    b = float(input("second number? "))
-    return a, b
+def enter_num():
+    num1 = float(input())
+    return num1
 
+def screen_options():
+    print("1: Add")
+    print("2: Subtract")
+    print("3: Multiply")
+    print("4: Divide")
+    print("5: Square")
+    print("6: Square Root")
+    print("7: Exponent")
+    print("8: Exit")
+    print("")
+    Operation = int(input('Choose an operation: (Select the number) '))
+    memStore = None
 
-def displayResult(x: float):
-    print(x, "\n")
+    if Operation == 1:
+        print("Add")
+        print("Enter the first number: ")
+        x = enter_num()
+        print("Enter the second number: ")
+        y = enter_num()
+        result = add(x, y)
+        return result
 
+    elif Operation == 2:
+        print("Subtract")
+        print('Enter the first number: ')
+        x = enter_num()
+        print('Enter the second number: ')
+        y = enter_num()
+        result = subtract(x, y)
+        return result
 
-def performCalcLoop(calc):
-    while True:
-        choice = input("Operation? ")
-        if choice == 'q':
-            break  # user types q to quit calulator.
-        elif choice == 'add':
-            a, b = getTwoNumbers()
-            displayResult(calc.add(a, b))
+    elif Operation == 3:
+        print('Multiply')
+        print('Enter the first number: ')
+        x = enter_num()
+        print('Enter the second number: ')
+        y = enter_num()
+        result = multiply(x, y)
+        return result
+
+    elif Operation == 4:
+        print("Divide")
+        print("Enter the first number: ")
+        x = enter_num()
+        print("Enter the second number: ")
+        y = enter_num()
+        if y == 0:
+            print("ERROR")
+            return None
         else:
-            print("That is not a valid input.")
+            result = divide(x, y)
+            return result
+
+    elif Operation == 5:
+        print("Square")
+        print('Enter a number to square: ')
+        base = enter_num()
+        result = square(base)
+        return result
+
+    elif Operation == 6:
+        print("Exponent")
+        print("Enter the base number: ")
+        x = enter_num()
+        print("Enter the exponent")
+        y = enter_num()
+        result = exp(x, y)
+        return result
+
+    elif Operation == 7:
+        print("Square Root")
+        print("Enter the number: ")
+        x = enter_num()
+        result = square_root(x)
+        return result
+
+    elif Operation == 8:
+        print("Inverse")
+        x = enter_num()
+        result = inv(x)
+        return result
+
+    elif Operation == 8:
+        condi = False
+        print("Thank you for pushing my buttons!")
 
 
-# main start
-def main():
-    calc = Calculator()
-    performCalcLoop(calc)
-    print("Done Calculating.")
+######################################################################
+## on screen ##
 
 
-if __name__ == '__main__':
-    main()
+print("Welcome to our Calculator!")
+print("How can we help you?")
+print("")
+
+screen_options()
