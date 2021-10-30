ff0defaafaf4d6441506f74abc04f9da29e96d80
Roethel Christine <roethelchristine@Dipintis-MacBook-Pro.local>
2021-10-30 18:39:48 -0400

    CR test 3

--- main-app.py
+++ main-app.py
@@ -8,8 +8,6 @@ def enter_num():
     num1 = float(input())
     return num1
 
-global mem
-mem = 0
 
 def enter_num():
     num1 = float(input())
@@ -25,7 +23,7 @@ def screen_options():
     print("7: Exponent      14: Inverse Tangent")
     print("")
     Operation = int(input('Choose an operation: (Select the number) '))
-    memStore = None
+    memStore = 0
 
     if Operation == 1:
         print("Add")
@@ -104,7 +102,7 @@ def screen_options():
         print("Sine")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.sine(x)
         return result
 
 
@@ -112,7 +110,7 @@ def screen_options():
         print("Cosine")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.cosine(x)
         return result
 
 
@@ -120,7 +118,7 @@ def screen_options():
         print("Tangent")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.tangent(x)
         return result
 
 
@@ -128,45 +126,47 @@ def screen_options():
         print("Inverse Sine")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.invsine(x)
         return result
 
     elif Operation == 13:
         print("Inverse Cosine")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.invcosine(x)
         return result
 
     elif Operation == 14:
         print("Inverse Tangent")
         print("Enter the number: ")
         x = enter_num()
-        result = calc.square_root(x)
+        result = calc.invtangent(x)
         return result
 
+##MEMORY FUNCTIONS
 
     elif Operation == 15:
         print("M+")
         print("Enter the number: ")
-        x = enter_num()
-        result = calc.square_root(x)
-        return result
+        memStore = float(input("Enter a value to store: "))
+        print(memStore)
+        return memStore
 
 
     elif Operation == 16:
         print("MC")
-        print("Enter the number: ")
-        x = enter_num()
-        result = calc.square_root(x)
-        return result
+        if type(memStore) != None:
+            print(memStore)
 
     elif Operation == 17:
         print("MCR")
-        print("Enter the number: ")
-        x = enter_num()
-        result = calc.square_root(x)
-        return result
+        store_choice = input("Do you want to clear memory? Y or N: ")
+        if store_choice == "y":
+            memStore = 0
+        else:
+            pass
+        print(memStore)
+        return memStore
 
     elif Operation == 18:
         condi = False
